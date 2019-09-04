79b079df3a213863e207f75a15085403e490440e
Krisli Dimo <krislidimo@gmail.com>
2019-09-04 17:42:31 -0500

    added Player class with pickUpItem function

--- src/player.py
+++ src/player.py
@@ -1,2 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+	def __init__(self, name, current_room):
+		self.name = name
+		self.current_room = current_room
+		self.items = []
+
+	def __str__(self):
+		print(f'name: {self.name}')
+		print(f'current_room: {self.current_room}')
+
+	def pickUpItem(item):
+		self.items.append(item)
